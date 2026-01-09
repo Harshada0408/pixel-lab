0c84805cce02fe9393b8aa17a149954ac099fe32
Harshada0408 <harshadaborse2125@gmail.com>
2026-01-09 18:37:44 +0530

    corrected now

---
+++ JS_1/js2.js/js.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    let conclusion;
+    if(bmi< 18.6){
+      conclusion = "you are under weight";
+    }
+    else if(bmi > 18.6 &&  bmi <24.9){
+       conclusion = "you are under normal range";
+    }
+    else if(bmi > 24.9){
+      conclusion = "you are overweight";
+    }
+    //show the result
+
+    results.innerHTML = `<span>${bmi} and ${conclusion}</span>`;
+  }
+});
+
